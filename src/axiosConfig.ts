acba92a9c566495f4fd9d9b1e22981a4a6e78022
NGO VAN MINH 20193012 <minh.nv193012@sis.hust.edu.vn>
2023-05-03 19:55:44 +0700

    fixbug

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -1,20 +1,20 @@
 import axios from 'axios';
 // require("dotenv").config();
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+    baseURL: 'https://api-projectsern.onrender.com',
 });
 // Add a request interceptor
 instance.interceptors.request.use(
-  (config) => {
-    const accessToken = localStorage.getItem('persist:auth');
+    (config) => {
+        const accessToken = localStorage.getItem('persist:auth');
 
-    // Do something before request is sent
-    return config;
-  },
-  async (error) => {
-    // Do something with request error
-    console.log(error);
-    return await Promise.reject(error);
-  },
+        // Do something before request is sent
+        return config;
+    },
+    async (error) => {
+        // Do something with request error
+        console.log(error);
+        return await Promise.reject(error);
+    },
 );
 export default instance;
